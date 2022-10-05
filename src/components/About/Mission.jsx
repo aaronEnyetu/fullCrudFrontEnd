7a6f41f8d37e28f2caecec86d252b53095dcc6af
Aaron Enyetu <aaronenyetu@gmail.com>
2022-10-05 11:19:35 -0700

    setup components, page layout and forms

--- src/components/About/Mission.jsx
+++
@@ -1,7 +0,0 @@
-export default function Mission() {
-  return (
-    <div>
-      <h2>Mission Statement Goes Here</h2>
-    </div>
-  );
-}
--- src/components/Projects/Projects.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Projects.css';
-
-export default function Projects() {
-  return (
-    <div className={styles.Projects}>
-      <h2>Project Details</h2>
-    </div>
-  );
-}
