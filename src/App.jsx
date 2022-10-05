5968a3c56dbace332c64e81a7aa4b327ac667879
Aaron Enyetu <aaronenyetu@gmail.com>
2022-10-05 12:34:08 -0700

    Setup Auth and State

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,30 @@ import {
 
 import Layout from './components/PageLayouts/Layout';
 import Dashboard from './components/Dashboard/Dashboard';
+import Auth from './components/Auth/Auth.jsx';
+import AuthForm from './components/Auth/AuthForm.jsx';
+import UserProvider from './state/UserContext.jsx';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Dashboard />} />
-        </Route>
+      <UserProvider>
+        <Routes>
+          <Route path="auth" element={<Auth />}>
+            <Route index element={<AuthForm mode="signin" />} />
+            <Route
+              path="signup"
+              element={<AuthForm mode="signup" />}
+            />
+          </Route>
 
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </UserProvider>
     </Router>
   );
 }
